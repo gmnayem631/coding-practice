229d7d118d4a04241d5da9a0bb0c4e4fba2ed056
gmnayem631 <gmnayem631@gmail.com>
2026-06-23 18:03:47 +0600

    replace orderQueue with orderHistory for better clarity and functionality

--- index.ts
+++ index.ts
@@ -19,29 +19,13 @@ const menu = [
 ];
 
 let cashInRegister = 100;
-const orderQueue = [];
+const orderHistory: Order[] = [];
 let id = 0;
-
-/**
- * Challenge: add a utility function called "addNewPizza" that takes a pizza object
- * and adds it to the menu
- */
 
 const addNewPizza = (pizza: Pizza) => {
   menu.push(pizza);
   return menu;
 };
-
-// console.log(addNewPizza({ name: "Dominos", price: 11 }));
-
-/**
- * Write another utility function, placeOrder, that takes a pizza name parameter and:
- * 1. finds that pizza object in the menu,
- * 2. adds the income to the cashInRegister
- * 3. pushes a new "order object" to the orderQueue
- *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
- * 4. returns the new order object (just in case we need it later)
- */
 
 function placeOrder(pizzaName: string) {
   const selectedPizza = menu.find(
@@ -55,29 +39,22 @@ function placeOrder(pizzaName: string) {
   cashInRegister = selectedPizza.price + cashInRegister;
 
   const newOrder = { pizza: selectedPizza, status: "ordered", id: id++ };
-  orderQueue.push(newOrder);
+  orderHistory.push(newOrder);
   return newOrder;
 }
 
 placeOrder("veggie");
 placeOrder("Hawaiian");
-// console.log(orderQueue);
-
-/**
- * Challenge: write another utility function, completeOrder, that takes an orderId (string) as a parameter
- * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
- * return the found order from the function.
- *
- * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You
- * can use a global `nextOrderId` variable and increment it every time a new order is created
- * to simulate real IDs being managed for us by a database.
- */
 
 function completeOrder(orderId: number) {
-  const order = orderQueue.find(
+  const order = orderHistory.find(
     (selectedOrder) => selectedOrder.id === orderId,
   );
-  order["status"] = "completed";
+  if (!order) {
+    console.error(`${orderId} does not exist`);
+    return;
+  }
+  order.status = "completed";
   return order;
 }
 
